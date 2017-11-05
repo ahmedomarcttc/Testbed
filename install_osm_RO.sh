2a2b583515102af443c92855a82a6afa75a38824
Ahmed Omar Shahidullah <ahmedomarshahidullah@Ahmeds-MacBook-Pro.local>
2017-11-05 06:52:56 +0100

    Adding OSM and VIM-EMU installation Bash file

---
+++ install_osm_RO.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Step 1: Updating & Installing make git python tox libmysqlclient-dev"
+sudo apt-get update
+sudo apt-get install -y make git python tox libmysqlclient-dev
+
+echo "Step 2: Creating Folder osm and cloning openvim to install lib_osm_openvim"
+if [ ! -d /home/$(whoami)/osm ]; then
+	mkdir osm;
+fi
+pushd osm
+git clone https://osm.etsi.org/gerrit/osm/openvim
+git -C openvim checkout 005a9dc  # this is temporal, because last version contains error at Makefile
+sudo make -C openvim lite
+popd
+# Use this line to check if it is installed and where
+sudo python -c 'import lib_osm_openvim; print lib_osm_openvim.__path__[0]'
+# Install database of ovim library
+OSMLIBOVIM_PATH=`python -c 'import lib_osm_openvim; print lib_osm_openvim.__path__[0]'`
+# -U and -P are the admin database user/password. Normally "-U root" without password "executed as root"
+sudo ${OSMLIBOVIM_PATH}/database_utils/install-db-server.sh -U root [-P passwd] -u mano -p manopw -d mano_vim_db --updatedb
+
+echo "Step 3: Cloning and installing OpenMANO RO"
+# RO (openmano)
+pushd osm
+git clone https://osm.etsi.org/gerrit/osm/RO.git
+cd RO
+git checkout v2.0
+sudo scripts/install-openmano.sh -u root -p root --noclone --forcedb --force --develop
+popd
